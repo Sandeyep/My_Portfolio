6f73dd25fb4d883a05c9388f650b67bc7d23b483
sandeyep <sandeyeppatel@gmail.com>
2024-05-31 20:44:45 +0545

    initial commit

--- nav.js
+++ nav.js
@@ -1,60 +1,12 @@
- // JavaScript to handle the click event and toggle the 'active' class
- document.addEventListener('DOMContentLoaded', function () {
-    var links = document.querySelectorAll('.nav ul li a');
+function initializeTyping(selector, strings) {
+  new Typed(selector, {
+      strings: strings,
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true,
+  });
+}
 
-    links.forEach(function (link) {
-        link.addEventListener('click', function () {
-            links.forEach(function (otherLink) {
-                otherLink.classList.remove('active');
-            });
-            this.classList.add('active');
-        });
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  initializeTyping(".typing", ["Student", "Front End Developer", "Graphic Designer",]);
 });
-
-
-
-// Typing animation script start
-var typed = new Typed(".typing", {
-    strings: ["Berojgar", "Front End Developer", "Youtuber", "Teacher"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  var typed = new Typed(".typing2", {
-    strings: ["React.js Developer", "Front End Developer", "Youtuber", "Teacher"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-//  slideer
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
-/*----------------------------------------------------------------- */
